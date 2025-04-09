80275386f3a00e98f1ccda08cbaebd77a156a450
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-04-09 12:05:55 +0100

    feat: update NewServer to include InvoiceService and configure invoice routes

--- go-gateway-api/internal/web/server/server.go
+++ go-gateway-api/internal/web/server/server.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gilbertouk/imersao-fullcycle-gateway-de-pagamento/go-gateway-api/internal/service"
 	"github.com/gilbertouk/imersao-fullcycle-gateway-de-pagamento/go-gateway-api/internal/web/handlers"
+	"github.com/gilbertouk/imersao-fullcycle-gateway-de-pagamento/go-gateway-api/internal/web/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -12,22 +13,34 @@ type Server struct {
 	router         *chi.Mux
 	server         *http.Server
 	accountService *service.AccountService
+	invoiceService *service.InvoiceService
 	port           string
 }
 
-func NewServer(accountService *service.AccountService, port string) *Server {
+func NewServer(accountService *service.AccountService, invoiceService *service.InvoiceService, port string) *Server {
 	return &Server{
 		router:         chi.NewRouter(),
 		accountService: accountService,
+		invoiceService: invoiceService,
 		port:           port,
 	}
 }
 
 func (s *Server) ConfigureRoutes() {
 	accountHendler := handlers.NewAccountHandler(s.accountService)
+	invoiceHandler := handlers.NewInvoiceHandler(s.invoiceService)
+	authMiddleware := middleware.NewAuthMiddleware(s.accountService)
 
 	s.router.Post("/accounts", accountHendler.Create)
 	s.router.Get("/accounts", accountHendler.Get)
+
+	s.router.Group(func(r chi.Router) {
+		r.Use(authMiddleware.Authenticate)
+		s.router.Post("/invoice", invoiceHandler.Create)
+		s.router.Get("/invoice/{id}", invoiceHandler.GetByID)
+		s.router.Get("/invoices", invoiceHandler.ListByAccount)
+	})
+
 }
 
 func (s *Server) Start() error {
