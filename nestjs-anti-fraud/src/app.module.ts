0130267cd4ca294f958988648526af6f19dd621f
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-04-14 21:10:18 +0100

    feat: configure global settings and import necessary modules in AppModule

--- nestjs-anti-fraud/src/app.module.ts
+++ nestjs-anti-fraud/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InvoicesModule } from './invoices/invoices.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        SUSPICIOUS_VARIATION_PERCENTAGE: Joi.number().required(),
+        INVOICES_HISTORY_COUNT: Joi.number().required(),
+        SUSPICIOUS_INVOICES_COUNT: Joi.number().required(),
+        SUSPICIOUS_TIMEFRAME_HOURS: Joi.number().required(),
+      }),
+    }),
+    InvoicesModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
