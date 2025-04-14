3fe9ec5c2f5db84def23eda4342e70852c6b3f3f
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-04-14 21:29:12 +0100

    feat: register InvoicesService and InvoicesController in InvoicesModule

--- nestjs-anti-fraud/src/invoices/invoices.module.ts
+++ nestjs-anti-fraud/src/invoices/invoices.module.ts
@@ -4,6 +4,8 @@ import { FrequentHighValueSpecification } from './fraud/specifications/frequent-
 import { SuspiciousAccountSpecification } from './fraud/specifications/suspicious-account.specification';
 import { UnusualAmountSpecification } from './fraud/specifications/unusual-amount.specification';
 import { FraudAggregateSpecification } from './fraud/specifications/fraud-aggregate.specification';
+import { InvoicesService } from './invoices.service';
+import { InvoicesController } from './invoices.controller';
 
 @Module({
   providers: [
@@ -31,6 +33,8 @@ import { FraudAggregateSpecification } from './fraud/specifications/fraud-aggreg
         UnusualAmountSpecification,
       ],
     },
+    InvoicesService,
   ],
+  controllers: [InvoicesController],
 })
 export class InvoicesModule {}
